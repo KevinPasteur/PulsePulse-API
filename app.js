ce376bfe37d1b7eaac0099e6db2fe3d35a550214
KevinPasteur <kevin.pasteur@icloud.com>
2023-11-22 10:48:16 +0100

    add mongoose connection, update users route

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ import createError from "http-errors";
 import logger from "morgan";
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose.connect(
+  process.env.DATABASE_URL ?? "mongodb://localhost:27017/pulsepulse"
+);
+
+if (process.env.NODE_ENV !== "test") {
+  mongoose.set("debug", true);
+}
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/api/v1/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
