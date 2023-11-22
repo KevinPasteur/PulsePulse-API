180aedf46501cf10939240f514348b22465d5a87
KevinPasteur <kevin.pasteur@icloud.com>
2023-11-22 10:49:06 +0100

    create test route for users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,20 @@
 import express from "express";
+import User from "../models/user.js";
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
   res.send("Got a response from the users route");
+});
+
+router.get("/test", function (req, res, next) {
+  User.find()
+    .sort("name")
+    .exec()
+    .then((users) => {
+      console.log(users);
+      res.send(users);
+    })
+    .catch(next);
 });
 
 export default router;
