55dc14c8bb396d33db6be31357d248730064b9e6
Martin Amez-Droz <martin.amezdroz@gmail.com>
2023-11-27 19:05:31 +0100

    added router for exercises

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
 app.use("/api/v1/users", usersRouter);
-app.use("/api/v1/exercises", usersRouter);
+app.use("/api/v1/exercises", exercisesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
