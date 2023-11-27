361bde90a436163d4e876095a231e681e36ad11c
Martin Amez-Droz <martin.amezdroz@gmail.com>
2023-11-27 19:06:00 +0100

    added function getExercises

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -1,1 +1,16 @@
+import asyncHandler from "express-async-handler";
+import Exercise from "../models/exercise.js";
+
 const createExercise = asyncHandler(async (req, res) => {});
+
+const getExercises = asyncHandler(async (req, res) => {
+    try {
+      const data = await Exercise.find().populate("creator");
+      console.log(data);
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  export { getExercises };
