15d065e1312a9a528b6c456c86589200c63dc67a
Maxime Cuche <maxime.cuche@heig-vd.ch>
2023-11-27 12:34:51 +0100

    Added new PUT route to update a user

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,27 @@ import {
   registerUser,
   loginUser,
   currentUser,
+  updateUser,
 } from "../controllers/userController.js";
-import authenticate from "../middleware/validateTokenHandler.js";
+import { authenticate, authorize } from "../middleware/validateTokenHandler.js";
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
   res.send("Got a response from the users route");
+});
+
+router.put("/:id", authenticate, function (req, res, next) {
+  User.findById(req.params.id)
+    .exec()
+    .then((user) => {
+      if (user.id === req.currentUserId || authorize("admin")) {
+        updateUser(req, res);
+      } else
+        return res
+          .status(400)
+          .send({ error: "You are not authorize to perform that" });
+    })
+    .catch(next);
 });
 
 router.post("/register", registerUser);
