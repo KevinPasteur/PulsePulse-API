f22f510ebadb21b0e6c72bfbc4b3bce2036dfd4c
Martin Amez-Droz <martin.amezdroz@gmail.com>
2023-11-27 19:05:08 +0100

    added route for getExercises

--- routes/exercises.js
+++ routes/exercises.js
@@ -1,5 +1,10 @@
 import express from "express";
 import Exercise from "../models/exercise.js";
+import { authenticate} from "../middleware/validateTokenHandler.js";
+import {
+    getExercises,
+  } from "../controllers/exerciseController.js";
 
 const router = express.Router();
+router.get("/", authenticate, getExercises);
 export default router;
