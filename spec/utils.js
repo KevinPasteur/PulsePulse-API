febf79c724adef4a22f5bb23269814fd4c4cd89c
Nathan Gretillat <nathan.gretillat@heig-vd.ch>
2023-11-27 17:04:30 +0100

    Ajout de la fonction generateValidAdminJwt

--- spec/utils.js
+++ spec/utils.js
@@ -15,3 +15,10 @@ export function generateValidJwt(user) {
   const claims = { sub: user._id.toString(), exp: exp };
   return signJwt(claims, jwtSecret);
 }
+
+export function generateValidAdminJwt(user) {
+  // Generate a valid Admin JWT which expires in 7 days.
+  const exp = (new Date().getTime() + 7 * 24 * 3600 * 1000) / 1000;
+  const claims = { sub: user._id.toString(), exp: exp, scope: "admin" };
+  return signJwt(claims, jwtSecret);
+}
