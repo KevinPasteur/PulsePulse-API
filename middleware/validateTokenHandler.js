7db4c6a3eac841e644fff9209921e87d2234a874
Maxime Cuche <maxime.cuche@heig-vd.ch>
2023-11-27 12:26:36 +0100

    Added authorize function

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -25,4 +25,22 @@ const authenticate = asyncHandler(async (req, res, next) => {
   }
 });
 
-export default authenticate;
+function authorize(requiredPermission) {
+  // Create an return an authorization middleware. The required permission
+  // will be available in the returned function because it is a closure.
+  return function authorizationMiddleware(req, res, next) {
+    if (!req.currentUserPermissions) {
+      // The user is not authenticated or has no permissions.
+      return res.sendStatus(403);
+    }
+    const authorized = req.currentUserPermissions.includes(requiredPermission);
+    if (!authorized) {
+      // The user is authenticated but does not have the required permission.
+      return res.sendStatus(403);
+    }
+    // The user is authorized.
+    next();
+  };
+}
+
+export { authenticate, authorize };
