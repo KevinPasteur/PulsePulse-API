dc9372428b608b15f0509d539de66827001d8675
Maxime Cuche <maxime.cuche@heig-vd.ch>
2023-11-27 12:25:20 +0100

    Added test "update user"

--- spec/users.spec.js
+++ spec/users.spec.js
@@ -26,10 +26,42 @@ describe("POST /api/v1/users", function () {
       .expect(201)
       .expect("Content-Type", /json/);
 
-      expect(res.body).toBeObject();
-      expect(res.body._id).toBeString();
-      expect(res.body.username).toEqual("JohnDoe");
-      expect(res.body.email).toEqual("JohnDoe@example.com");
-      expect(res.body).toContainAllKeys(["_id", "username", "email"]);
+    expect(res.body).toBeObject();
+    expect(res.body._id).toBeString();
+    expect(res.body.username).toEqual("JohnDoe");
+    expect(res.body.email).toEqual("JohnDoe@example.com");
+    expect(res.body).toContainAllKeys(["_id", "username", "email"]);
+  });
+});
+
+describe("PUT /api/v1/users/:id", function () {
+  let johnDoe;
+
+  beforeEach(async function () {
+    const hashedPassword = await bcrypt.hash("1234", 10);
+    // Create 1 user before update informations.
+    [johnDoe] = await Promise.all([
+      User.create({
+        username: "johnDoe",
+        email: "johnDoe@example.com",
+        password: hashedPassword,
+        role: "user",
+      }),
+    ]);
+  });
+
+  it("should update an user", async function () {
+    const token = await generateValidJwt(johnDoe);
+    await supertest(app)
+      .put("/api/v1/users/" + johnDoe.id)
+      .auth(token, { type: "bearer" })
+      .send({
+        email: "JohnDoee@example.com",
+      })
+      .expect(200)
+      .expect("Content-Type", /json/);
+
+    const user = await User.findOne({ username: johnDoe.username });
+    expect(user.email).toEqual("JohnDoee@example.com");
   });
 });
