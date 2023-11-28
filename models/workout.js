6e69b0b804c31bf5ff5d6c9eb3f56b8eecddcc3f
KevinPasteur <kevin.pasteur@icloud.com>
2023-11-28 10:40:48 +0100

    Add workout model

---
+++ models/workout.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+// Define the schema for workouts
+const workoutSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    isPublic: {
+      type: Boolean,
+      required: true,
+    },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    exercises: [{ type: Schema.Types.ObjectId, ref: "Exercise" }],
+  },
+  { timestamps: true }
+);
+
+workoutSchema.set("toJSON", {
+  transform: transformJsonUser,
+});
+
+function transformJsonUser(doc, json, options) {
+  // Remove the hashed password from the generated JSON.
+  delete json.password;
+  delete json.__v;
+  return json;
+}
+
+// Create the model from the schema and export it
+export default mongoose.model("Workout", workoutSchema);
