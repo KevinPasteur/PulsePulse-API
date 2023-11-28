65dd0c8fe086f6b45374742a0dbf48069e5407dd
KevinPasteur <kevin.pasteur@icloud.com>
2023-11-28 10:49:56 +0100

    add workoutController

---
+++ controllers/workoutController.js
@@ -0,0 +1,6 @@
+import asyncHandler from "express-async-handler";
+import Workout from "../models/workout.js";
+
+const createWorkout = asyncHandler(async (req, res) => {});
+
+export { createWorkout };
