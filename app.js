c0115b14a59b3e1a7a16e25af38d888796b97ede
Kévin <kevin.pasteur@icloud.com>
2023-11-28 16:10:20 +0100

    Add swagger serve

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ import usersRouter from "./routes/users.js";
 import exercisesRouter from "./routes/exercises.js";
 import workoutsRouter from "./routes/workouts.js";
 import mongoose from "mongoose";
+import fs from "fs";
+import yaml from "js-yaml";
+import swaggerUi from "swagger-ui-express";
 import "dotenv/config";
 
 const app = express();
@@ -32,6 +35,11 @@ app.use("/", indexRouter);
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/exercises", exercisesRouter);
 app.use("/api/v1/workouts", workoutsRouter);
+
+// Parse the OpenAPI document.
+const openApiDocument = yaml.load(fs.readFileSync("./openapi.yml"));
+// Serve the Swagger UI documentation.
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openApiDocument));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
