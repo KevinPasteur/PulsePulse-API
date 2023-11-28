db6eeeeb93571a5959c97760049c31596e70f928
KevinPasteur <kevin.pasteur@icloud.com>
2023-11-28 10:47:56 +0100

    Add test route to broadcast message with WebSocket

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,8 +1,15 @@
 import express from "express";
+import { broadcastMessage } from "../ws.js";
+
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
-  res.send("Workout!");
+router.post("/", function (req, res, next) {
+  try {
+    broadcastMessage({ hello: "world" });
+    return res.status(200).send({ message: "Broadcast success" });
+  } catch (err) {
+    return res.status(400).send({ error: err });
+  }
 });
 
 export default router;
