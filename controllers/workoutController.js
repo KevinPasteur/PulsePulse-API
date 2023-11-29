dba6a2af7d317ed88c3fca531cdcb02d90f3bf2a
KevinPasteur <kevin.pasteur@icloud.com>
2023-11-29 18:09:21 +0100

    add getWorkouts and getPublicWorkouts

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -3,4 +3,65 @@ import Workout from "../models/workout.js";
 
 const createWorkout = asyncHandler(async (req, res) => {});
 
-export { createWorkout };
+const getWorkouts = asyncHandler(async (req, res) => {
+  Workout.aggregate([
+    {
+      $project: {
+        name: 1,
+        description: 1,
+        creator: 1,
+        exercises: 1,
+        isPublic: 1,
+        totalExercises: {
+          $cond: {
+            if: { $isArray: "$exercises" },
+            then: { $size: "$exercises" },
+            else: "NA",
+          },
+        },
+      },
+    },
+  ])
+    .then((results) => {
+      res.send(results);
+    })
+    .catch(() => {
+      res.status(400).send({
+        message: "Workout not found",
+      });
+    });
+});
+
+const getPublicWorkouts = asyncHandler(async (req, res) => {
+  Workout.aggregate([
+    {
+      $match: { isPublic: true },
+    },
+
+    {
+      $project: {
+        name: 1,
+        description: 1,
+        creator: 1,
+        exercises: 1,
+        totalExercises: {
+          $cond: {
+            if: { $isArray: "$exercises" },
+            then: { $size: "$exercises" },
+            else: "NA",
+          },
+        },
+      },
+    },
+  ])
+    .then((results) => {
+      res.send(results);
+    })
+    .catch(() => {
+      res.status(400).send({
+        message: "Workout not found",
+      });
+    });
+});
+
+export { createWorkout, getWorkouts, getPublicWorkouts };
